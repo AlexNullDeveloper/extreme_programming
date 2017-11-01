0b0315f91b127d963d716180f97558f0d51ae5e2
a.talismanov <a.talismanov@creditpilot.ru>
2017-11-01 12:04:48 +0300

    initial commit

---
+++ src/main/java/com/talismanov/Sum.java
@@ -0,0 +1,29 @@
+package com.talismanov;
+
+
+public class Sum implements Expression {
+    Expression augend;
+    Expression addend;
+
+    Sum(Expression augend, Expression addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+//    public Money reduce(Bank bank, String to) {
+//        int amount = augend.amount + addend.amount;
+//        return new Money(amount, to);
+//    }
+
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.reduce(bank, to).amount
+                + addend.reduce(bank, to).amount;
+        return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend) {
+        return null;
+    }
+
+
+}
